8f590d4b072fda5e02bf5cdb8d567b79d137e412
Zizhen Guo <gzen30@outlook.com>
2024-05-20 16:06:14 -0700

    load image

--- memberCard.js
+++ memberCard.js
@@ -9,7 +9,17 @@ export class MemberCard {
 
 	newCard(document) {
 		let card = document.createElement("div");
-		card.textContent = `Name: ${this.name}`; 
+
+		let nameText = document.createElement("p");
+		nameText.textContent = `Name: ${this.name}`;
+		card.appendChild(nameText);
+
+		let imgContent = document.createElement("img");
+		imgContent.src = this.img;
+		imgContent.alt = this.name;
+		imgContent.referrerPolicy = "no-referrer"; // magic code to load image from web
+		card.appendChild(imgContent);
+
 		const cardsDiv = document.getElementById("cards");
 		cardsDiv.appendChild(card);
 	}
